c748473fe0e99b9a72243c72cf3b4e8cf00828b5
Mathieu Méa <mathieu.mea@gmail.com>
2021-04-29 17:14:44 -0400

    New scripts

---
+++ pidcat_mt_release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ -z "${ANDROID_HOME}" ]]; then
+    echo "ANDROID_HOME not set!";
+    exit 1;
+fi
+PIDCAT="pidcat.py";
+if [[ ! -f "$ANDROID_HOME/$PIDCAT" ]]; then
+    echo "'$PIDCAT' does not exist in '$ANDROID_HOME'!"
+    exit 1;
+fi
+$ANDROID_HOME/$PIDCAT org.mtransit.android
