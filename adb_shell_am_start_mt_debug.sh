88c4fa5290ea4a9847febe528a13b507113187ca
Mathieu Méa <mathieu.mea@gmail.com>
2022-12-02 10:57:50 -0500

    Add scripts for starting MT debug app.

---
+++ adb_shell_am_start_mt_debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ -z "${ANDROID_HOME}" ]]; then
+    echo "ANDROID_HOME not set!";
+    exit 1;
+fi
+$ANDROID_HOME/platform-tools/adb shell am start \
+-n org.mtransit.android.debug/org.mtransit.android.ui.SplashScreenActivity \
+$@ \
+;
